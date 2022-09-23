06a2774598934adf9292d6955e1ceb3bc6047b18
Layne Rupar <100589700+laru852@users.noreply.github.com>
2022-09-23 14:42:39 +0000

    9/23/22

--- playground.py
+++ playground.py
@@ -1,14 +1,41 @@
-employees = ["Alice", "Vera", "Flo", "Mel"]
+employees = ["Bruce", "Oswald", "Flo", "Bif"]
 
 for employee in employees:
     print(employee)
     employees.sort()
 
-employees.append("Layne")
-employees.remove("Flo")
+employees.append("Layne") #adding a new employee
+employees.remove("Flo") #removing an employee
+
+print() #space
+
+for employee in employees:
+    print(employee)
+    employees.sort()
+
+Layne = "Baller"
 
 print()
 
-for employee in employees:
-    print(employee)
-    employees.sort()
+if Layne == "Baller":
+    print("We ball")
+else: 
+    print("Gabe isn't real")
+
+Layne = "Not Baller"
+
+print() #space
+
+if Layne == "Baller":
+    print("We ball")
+else: 
+    print("Gabe isn't real")
+
+    betterPerson = input("Who is a better person? Gabe or Layne?")
+    userGuess = 1
+
+    while betterPerson != "Layne":
+        userGuess = userGuess + 1
+        betterPerson = input("Guess again silly.")
+
+        print("You are so smart")
